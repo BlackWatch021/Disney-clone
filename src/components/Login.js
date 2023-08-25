80939bd10f72603a98fb8358592d97ae40d9bbdd
Himanshu Negi <himanshu.21negi@gmail.com>
2023-08-25 19:52:33 +0530

    Login settings changed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,69 @@
-import React from 'react';
-import styled from 'styled-components'
-
+import React from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { auth, provider } from "../firebase";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setSignOutState,
+  setUserLoginDetails,
+} from "../features/userSlice";
+import { useNavigate } from "react-router";
 
 const Login = (props) => {
-  return <Container>
-    <Content>
-      <CTA>
-        <CTALogoOne src='/images/cta-logo-one.svg' />
-        <SignUp>GET ALL THERE</SignUp>
-        <Description>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1</Description>
-        <CTALogoTwo src='/images/cta-logo-two.png' />
-      </CTA>
-      <Bgimg />
-    </Content>
-  </Container>
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  const navigate = useNavigate();
+
+  const handleAuth = (props) => {
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          navigate("/");
+        })
+        .catch((err) => alert(err.message));
+    }
+  };
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
+
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" />
+          <SignUp onClick={handleAuth}>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" />
+        </CTA>
+        <Bgimg />
+      </Content>
+    </Container>
+  );
 };
-
-
 
 const Container = styled.section`
   overflow: hidden;
@@ -25,8 +72,6 @@ const Container = styled.section`
   text-align: center;
   height: 100vh;
 `;
-
-
 
 const Content = styled.div`
   margin-bottom: 10vw;
@@ -42,9 +87,6 @@ const Content = styled.div`
   height: 100%;
 `;
 
-
-
-
 const Bgimg = styled.div`
   height: 100%;
   background-position: top;
@@ -58,67 +100,55 @@ const Bgimg = styled.div`
   z-index: -1;
 `;
 
-
-
 const CTA = styled.div`
-margin-bottom: 2vw;
-max-width: 650px;
+  margin-bottom: 2vw;
+  max-width: 650px;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin:auto;
-`
+  margin: auto;
+`;
 const CTALogoOne = styled.img`
-max-width: 600px;
-max-height: contain;
-display: block;
-margin-bottom: 12px;
-width: 100%;
-`
-
+  max-width: 600px;
+  max-height: contain;
+  display: block;
+  margin-bottom: 12px;
+  width: 100%;
+`;
 
 const SignUp = styled.button`
-font-weight: bold;
-color:#f9f9f9;
-background-color:#0063e5;
-margin-bottom: 12px;
-width: 100%;
-font-size: 18px;
-letter-spacing: 1.5px;
-padding: 16.5px 0;
-border: 1px solid transparent;
-border-radius: 4px;
-cursor: pointer;
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  font-size: 18px;
+  letter-spacing: 1.5px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  cursor: pointer;
 
-
-:hover{
-  background-color:#0483ee ;
-}
-`
-
-
+  :hover {
+    background-color: #0483ee;
+  }
+`;
 
 const Description = styled.h2`
-color:hsla(0,0%,95.3%,1);
-font-size: 11px;
-margin: 0 0 24px;
-line-height: 1.5;
-letter-spacing: 1.5px;
-`
-
-
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
 
 const CTALogoTwo = styled.img`
-max-width: 600px;
-margin-bottom: 20px;
-display: inline-block;
-vertical-align: bottom;
-width: 100%;
-`
-
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
 
 export default Login;
-
-
-
